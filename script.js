78f4e9c5cd1966ec76ebc6644767eb049cdf838b
PraptiH <praptiantara1892@gmail.com>
2026-04-16 11:35:10 +0600

    Design the work section

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const customCursor = document.querySelector(".customCursor")
 const customCursor2 = document.querySelector(".customCursor2")
 const navlink = document.querySelectorAll(".navlink")
+const parentBox = document.querySelector(".parentBox")
+const outerBox = document.querySelector(".outerBox")
+const innerBox = document.querySelector(".innerBox")
 
 let smallMouseX = 0, smallMouseY = 0
 let bigMouseX = 0, bigMouseY = 0
@@ -37,6 +40,43 @@ navlink.forEach(link => {
 
         customCursor2.style.transform = "scale(1)"
         customCursor.style.display = "block"
+    })
+})
+
+parentBox.addEventListener('mouseenter',()=>{
+    console.log("mouse")
+    outerBox.animate([
+        {transform: 'rotate(180deg)'}
+    ],{
+        duration:1000,
+        iteration:1
+
+    })
+    innerBox.animate([
+        {transform: 'scale(1.8)'}
+    ],{
+        duration:1000,
+        iteration:1,
+        fill:'forwards'
+    })
+})
+
+parentBox.addEventListener('mouseleave',()=>{
+    console.log("mouse")
+    outerBox.animate([
+        // {transform: 'rotate(0deg)'},
+        {transform: 'rotate(-180deg)'}
+    ],{
+        duration:1000,
+        iteration:1,
+        animationTimingFunction:"ease"
+    })
+    innerBox.animate([
+        {transform: 'scale(1)'}
+    ],{
+        duration:1000,
+        iteration:1,
+        fill:'forwards'
     })
 })
 
